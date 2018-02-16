c628da8d59d1fa414350964e8ac56adc6ccd1578
John D. Fisher <jdfenw@gmail.com>
2018-02-16 13:21:22 -0800

    feat: Disable notification batter not need action

--- Test-IsOnBattery.ps1
+++ Test-IsOnBattery.ps1
@@ -10,15 +10,15 @@ Function Test-IsOnBattery{
 
 $FullChargedCapacity = (Get-WmiObject -Class BatteryFullChargedCapacity -Namespace root\wmi -ComputerName $computer).FullChargedCapacity
 $RemainingCapacity = (Get-WmiObject -Class BatteryStatus -Namespace root\wmi -ComputerName $computer).RemainingCapacity
-$iPerecent = [int](($RemainingCapacity / $FullChargedCapacity * 100 ) % 100)
+$iPercent = [int](($RemainingCapacity / $FullChargedCapacity * 100 ) % 100)
 
 while ($true) {
-    If (Test-IsOnBattery($computer) -AND ($iPerecent -gt 75)) {
-        $dummy = [System.Windows.Forms.MessageBox]::Show('Battery is fully charged') }
-    ElseIf (-NOT (Test-IsOnBattery($computer)) -AND ($iPerecent -lt 50)) {
-        $dummy = [System.Windows.Forms.MessageBox]::Show('Please charge Battery') }
-    Else {
-        $dummy = [System.Windows.Forms.MessageBox]::Show('Battery is OK')
-    }
+    If (Test-IsOnBattery($computer) -AND ($iPercent -gt 75)) {
+        [System.Windows.Forms.MessageBox]::Show('Battery is fully charged') }
+    ElseIf (-NOT (Test-IsOnBattery($computer)) -AND ($iPercent -lt 50)) {
+        [System.Windows.Forms.MessageBox]::Show('Please charge Battery') }
+    # Else {
+    #     [System.Windows.Forms.MessageBox]::Show('Battery is OK')
+    # }
     Start-Sleep -Seconds 600
 }
