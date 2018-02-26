cbb3e7802c284e821dbe3008fbd012899d1ea4cb
John D. Fisher <jdfenw@gmail.com>
2018-02-25 17:33:05 -0800

    feat: Install BurntToast for ScheduledTask

--- ScheduleTask.ps1
+++ ScheduleTask.ps1
@@ -1,2 +1,11 @@
-﻿$xml_path = (Get-ChildItem ".\" "Battery Monitor.xml").FullName
+﻿try {
+    Import-Module BurntToast
+    Write-Host "Module BurntToast Installed"
+} catch {
+    Write-Host "Installing BurntToast"
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Install-Module -Scope CurrentUser -Name BurntToast
+}
+
+$xml_path = (Get-ChildItem ".\" "Battery Monitor.xml").FullName
 Register-ScheduledTask -Xml (get-content "$xml_path" | out-string) -TaskName "Battery Monitor" -User $env:USERNAME
