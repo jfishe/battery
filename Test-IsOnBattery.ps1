148ee84b02f704acfba10a0a12d572fd87c71b06
John D. Fisher <jdfenw@gmail.com>
2018-02-25 16:38:54 -0800

    fix: IsOnBattery logic and if statements
    Change Test-IsOnBattery to Test-PowerOnLine, which is what the function
    actually returns as a BOOL.
    
    Assign inverse of function return to $IsOnBattery and use in If
    statements.

--- Test-IsOnBattery.ps1
+++ Test-IsOnBattery.ps1
@@ -3,23 +3,26 @@ Param($computer = "localhost")
 # add the required .NET assembly
 Add-Type -AssemblyName System.Windows.Forms
 
-Function Test-IsOnBattery{
+Function Test-PowerOnLine{
     Param([string]$computer)
     [BOOL](Get-WmiObject -Class BatteryStatus -Namespace root\wmi -ComputerName $computer).PowerOnLine
-} # end function test-IsOnBattery
+} # end function test-PowerOnLine
 
 $FullChargedCapacity = (Get-WmiObject -Class BatteryFullChargedCapacity -Namespace root\wmi -ComputerName $computer).FullChargedCapacity
 
 while ($true) {
+    $IsOnBattery = -NOT (Test-PowerOnLine($computer))
+
     $RemainingCapacity = (Get-WmiObject -Class BatteryStatus -Namespace root\wmi -ComputerName $computer).RemainingCapacity
     $iPercent = [int](($RemainingCapacity / $FullChargedCapacity * 100 ) % 100)
 
-    If (Test-IsOnBattery($computer) -AND ($iPercent -gt 75)) {
-        [System.Windows.Forms.MessageBox]::Show('Battery is fully charged') }
-    ElseIf (-NOT (Test-IsOnBattery($computer)) -AND ($iPercent -lt 50)) {
-        [System.Windows.Forms.MessageBox]::Show('Please charge Battery') }
+    If (($IsOnBattery) -AND ($iPercent -lt 50)) {
+            [System.Windows.Forms.MessageBox]::Show('Please charge Battery') }
+    ElseIf (($iPercent -gt 75) -AND (-NOT ($IsOnBattery))) {
+        [System.Windows.Forms.MessageBox]::Show('Unplug. Battery is fully charged') }
     # Else {
     #     [System.Windows.Forms.MessageBox]::Show('Battery is OK')
     # }
+
     Start-Sleep -Seconds 600
 }
