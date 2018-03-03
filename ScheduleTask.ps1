1ef7aa19d0a8556273152c23a23d3cd44d420dfa
John D. Fisher <jdfenw@gmail.com>
2018-03-02 19:16:47 -0800

    feat: Catch scheduled task errors
    Add specific exceptions for import-module failure.
    
    Add module update check for BurntToast.
    
    Catch error when Register-ScheduledTask already exists. Allow user to
    delete and re-install.

--- ScheduleTask.ps1
+++ ScheduleTask.ps1
@@ -1,11 +1,22 @@
-﻿try {
+﻿Try {
     Import-Module BurntToast
-    Write-Host "Module BurntToast Installed"
-} catch {
+    Write-Host "Module BurntToast Installed."
+} Catch [System.IO.FileNotFoundException] {
     Write-Host "Installing BurntToast"
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     Install-Module -Scope CurrentUser -Name BurntToast
 }
 
+Write-Host "Checking for Updates"
+Update-Module BurntToast
+
 $xml_path = (Get-ChildItem ".\" "Battery Monitor.xml").FullName
-Register-ScheduledTask -Xml (get-content "$xml_path" | out-string) -TaskName "Battery Monitor" -User $env:USERNAME
+
+Try {
+    Register-ScheduledTask -Xml (get-content "$xml_path" | out-string) -TaskName "Battery Monitor" -User $env:USERNAME -ErrorAction Stop
+} Catch  [Microsoft.Management.Infrastructure.CimException] {
+    Write-Host "Select Yes to register a new Battery Monitor"
+    Write-Host "Select No to Cancel."
+    Unregister-ScheduledTask -TaskName "Battery Monitor" -Confirm
+    Register-ScheduledTask -Xml (get-content "$xml_path" | out-string) -TaskName "Battery Monitor" -User $env:USERNAME
+}
