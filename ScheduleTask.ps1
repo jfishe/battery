8c2dd51fd9f71cda463e84fc4aa44ca91172a9b5
John D. Fisher <jdfenw@gmail.com>
2018-03-10 16:48:25 -0800

    feat: Resolve computer & user ID
    Replace xml.Task.Triggers.LogonTrigger.UserId with values from
    environment variables COMPUTERNAME\USERNAME. Save to a temporary XML
    file and Register-ScheduledTask.

--- ScheduleTask.ps1
+++ ScheduleTask.ps1
@@ -12,11 +12,19 @@ Update-Module BurntToast
 
 $xml_path = (Get-ChildItem ".\" "Battery Monitor.xml").FullName
 
+# Update computer name and user name in Task Scheduler XML file
+[xml]$xml = Get-Content $xml_path
+$xml.Task.Triggers.LogonTrigger.UserId = "$env:COMPUTERNAME\$env:USERNAME"
+$xml_tmp = (New-TemporaryFile | Rename-Item -NewName {$_ -replace 'tmp$', 'xml' } -PassThru).FullName
+$xml.Save("$xml_tmp")
+
 Try {
-    Register-ScheduledTask -Xml (get-content "$xml_path" | out-string) -TaskName "Battery Monitor" -User $env:COMPUTERNAME\$env:USERNAME  -ErrorAction Stop
+    Register-ScheduledTask -Xml (get-content "$xml_tmp" | out-string) -TaskName "Battery Monitor" -User $env:COMPUTERNAME\$env:USERNAME  -ErrorAction Stop
 } Catch  [Microsoft.Management.Infrastructure.CimException] {
     Write-Host "Select Yes to register a new Battery Monitor"
     Write-Host "Select No to Cancel."
     Unregister-ScheduledTask -TaskName "Battery Monitor" -Confirm
-    Register-ScheduledTask -Xml (get-content "$xml_path" | out-string) -TaskName "Battery Monitor" -User $env:COMPUTERNAME\$env:USERNAME
+    Register-ScheduledTask -Xml (get-content "$xml_tmp" | out-string) -TaskName "Battery Monitor" -User $env:COMPUTERNAME\$env:USERNAME
 }
+
+Remove-Item $xml_tmp
