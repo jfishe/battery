107f6bfd7cb60b9dbce44b1a13c8a53cab836265
John D. Fisher <jdfenw@gmail.com>
2018-03-10 16:20:51 -0800

    fix: Add Import-Module -ErrorAction

--- ScheduleTask.ps1
+++ ScheduleTask.ps1
@@ -1,5 +1,5 @@
 ﻿Try {
-    Import-Module BurntToast
+    Import-Module BurntToast -ErrorAction Stop
     Write-Host "Module BurntToast Installed."
 } Catch [System.IO.FileNotFoundException] {
     Write-Host "Installing BurntToast"
@@ -13,10 +13,10 @@ Update-Module BurntToast
 $xml_path = (Get-ChildItem ".\" "Battery Monitor.xml").FullName
 
 Try {
-    Register-ScheduledTask -Xml (get-content "$xml_path" | out-string) -TaskName "Battery Monitor" -User $env:USERNAME -ErrorAction Stop
+    Register-ScheduledTask -Xml (get-content "$xml_path" | out-string) -TaskName "Battery Monitor" -User $env:COMPUTERNAME\$env:USERNAME  -ErrorAction Stop
 } Catch  [Microsoft.Management.Infrastructure.CimException] {
     Write-Host "Select Yes to register a new Battery Monitor"
     Write-Host "Select No to Cancel."
     Unregister-ScheduledTask -TaskName "Battery Monitor" -Confirm
-    Register-ScheduledTask -Xml (get-content "$xml_path" | out-string) -TaskName "Battery Monitor" -User $env:USERNAME
+    Register-ScheduledTask -Xml (get-content "$xml_path" | out-string) -TaskName "Battery Monitor" -User $env:COMPUTERNAME\$env:USERNAME
 }
