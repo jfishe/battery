71cf117023d9c29b47fb6c067649ad9cea3ab1cc
John D. Fisher <jdfenw@gmail.com>
2018-05-18 21:52:33 -0700

    build: Add PSDeploy and build help xml

---
+++ PSDeploy.ps1
@@ -0,0 +1,49 @@
+
+<#PSScriptInfo
+
+.VERSION 0.2.3
+
+.GUID de439f5f-af3a-4273-ac2c-8d9cce3ff19f
+
+.AUTHOR jdfenw@gmail.com
+
+.COMPANYNAME John D. Fisher
+
+.COPYRIGHT Copyright (c) 2018 John D. Fisher
+
+.TAGS 
+
+.LICENSEURI https://github.com/jfishe/battery/blob/master/LICENSE.md
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES PlatyPS
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ PlayPS Deploy Script 
+
+#> 
+
+Deploy ScheduleTask_Test-IsOnBattery {
+    By PlatyPS {
+        FromSource 'docs'
+        To "ScheduleTask_Test-IsOnBattery\en-US"
+        Tagged Help, Module
+        WithOptions @{
+            Force = $true
+        }
+    }
+}
